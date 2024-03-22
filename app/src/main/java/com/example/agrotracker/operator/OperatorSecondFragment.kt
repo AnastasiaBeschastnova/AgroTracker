c626ce4b3f2345ce7ac5dfd92caa98ebec023d76
AnastasiaBeschastnova <baugold@mail.ru>
2024-03-22 18:55:47 +0300

    Added a "temporary" display of parameters by type of work

--- app/src/main/java/com/example/agrotracker/operator/OperatorSecondFragment.kt
+++ app/src/main/java/com/example/agrotracker/operator/OperatorSecondFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.agrotracker.R
 import com.example.agrotracker.databinding.FragmentOperatorSecondBinding
@@ -21,8 +24,8 @@ class OperatorSecondFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentOperatorSecondBinding.inflate(inflater, container, false)
@@ -32,7 +35,32 @@ class OperatorSecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //пробная проверка отображения дополнительных параметров в зависимости от типа
+        binding.worktypeInputEditText.doAfterTextChanged {
 
+
+            if (binding.worktypeInputEditText.text.toString() == "посев") {
+                binding.culture.isVisible = true
+                binding.cultureTextInputLayout.isVisible = true
+                //binding.cultureInputEditText.isVisible = true
+                //println("if")
+            } else {
+                //очистить поле и сделать его невидимым
+                binding.culture.isVisible = false
+                binding.cultureTextInputLayout.isVisible = false
+                //binding.cultureInputEditText.isVisible = false
+                //binding.cultureInputEditText.setText("")
+                //println("else")
+
+            }
+
+            //println(binding.worktypeInputEditText.text.toString())
+            //print(binding.cultureInputEditText.text.toString())
+        }
+        binding.cultureInputEditText.doAfterTextChanged {
+            //println(binding.cultureInputEditText.text.toString())
+
+        }
         binding.buttonStart.setOnClickListener {
             findNavController().navigate(R.id.action_operatorSecondFragment_to_operatorThirdFragment)
         }
