a2bf64c74a61a5c97a7c528404a4496f401dbc2b
AnastasiaBeschastnova <baugold@mail.ru>
2024-04-23 01:35:13 +0300

    Changed the method of obtaining geolocation on the ContinueWorkFragment

--- app/src/main/java/com/example/agrotracker/operator/ContinueWorkFragment.kt
+++ app/src/main/java/com/example/agrotracker/operator/ContinueWorkFragment.kt
@@ -7,6 +7,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,6 +24,11 @@ import com.example.agrotracker.api.requests.InsertPointRequest
 import com.example.agrotracker.api.requests.InsertWorkRequest
 import com.example.agrotracker.api.requests.UpdateWorkRequest
 import com.example.agrotracker.databinding.FragmentContinueWorkBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -124,40 +130,81 @@ class ContinueWorkFragment : Fragment() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            val locationManager =
-                requireActivity().application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                1000L,
-                1f,
-                LocationListener { location: Location? ->
-                    if (location != null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
-                        val startPoint = GeoPoint(location.latitude, location.longitude)
-                        val startMarker = Marker(binding.mapview)
-                        startMarker.setPosition(startPoint)
-                        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                        binding.mapview.getOverlays().clear()
-                        binding.mapview.getOverlays().add(startMarker)
-                        binding.mapview.controller.setCenter(startPoint)
-                        binding.loadingText.isVisible=false
-                        binding.geo.isVisible=true
-                        binding.mapview.isVisible=true
-                        binding.workId.isVisible=true
-                        //binding.workTime.isVisible=true
-                        binding.geo.text ="Местоположение: \n"+location.latitude.toString()+" (ш.),\n"+location.longitude.toString()+" (д.)"
-                        val point_time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date()).toString()
-                        workId?.let {
-                            insertPoint(it, location.latitude, location.longitude, point_time)
-                        }
+            val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            fusedLocationProviderClient.requestLocationUpdates(
+                LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).setWaitForAccurateLocation(false)
+                    .setMinUpdateIntervalMillis(500)
+                    .setMaxUpdateDelayMillis(1000)
+                    .build(),
+                object : LocationCallback() {
+                    override fun onLocationResult(p0: LocationResult) {
+                        p0 ?: return
+                        for (location in p0.locations) {
+                            if (location != null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
+                                val startPoint = GeoPoint(location.latitude, location.longitude)
+                                val startMarker = Marker(binding.mapview)
+                                startMarker.setPosition(startPoint)
+                                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                                binding.mapview.getOverlays().clear()
+                                binding.mapview.getOverlays().add(startMarker)
+                                binding.mapview.controller.setCenter(startPoint)
+                                binding.loadingText.isVisible=false
+                                binding.geo.isVisible=true
+                                binding.mapview.isVisible=true
+                                binding.workId.isVisible=true
+                                //binding.workTime.isVisible=true
+                                binding.geo.text ="Местоположение: \n"+location.latitude.toString()+" (ш.),\n"+location.longitude.toString()+" (д.)"
+                                val point_time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date()).toString()
+                                workId?.let {
+                                    insertPoint(it, location.latitude, location.longitude, point_time)
+                                }
 
+                            }
+                            else if(location == null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)){
+                                val boundingBox = BoundingBox(geoVlg.altitude*1.01, geoVlg.longitude*1.01, geoVlg.altitude*0.99, geoVlg.longitude*0.99)
+                                binding.mapview.zoomToBoundingBox(boundingBox.increaseByScale(0.05f), false)
+                                binding.mapview.controller.setCenter(geoVlg)
+                                binding.geo.text ="Местоположение: не распознано"
+                            }
+                        }
                     }
-                    else if(location == null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)){
-                        val boundingBox = BoundingBox(geoVlg.altitude*1.01, geoVlg.longitude*1.01, geoVlg.altitude*0.99, geoVlg.longitude*0.99)
-                        binding.mapview.zoomToBoundingBox(boundingBox.increaseByScale(0.05f), false)
-                        binding.mapview.controller.setCenter(geoVlg)
-                        binding.geo.text ="Местоположение: не распознано"
-                    }
-                })
+                },
+                Looper.getMainLooper()
+                )
+//            val locationManager =
+//                requireActivity().application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//            locationManager.requestLocationUpdates(
+//                LocationManager.GPS_PROVIDER,
+//                1000L,
+//                1f,
+//                LocationListener { location: Location? ->
+//                    if (location != null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
+//                        val startPoint = GeoPoint(location.latitude, location.longitude)
+//                        val startMarker = Marker(binding.mapview)
+//                        startMarker.setPosition(startPoint)
+//                        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+//                        binding.mapview.getOverlays().clear()
+//                        binding.mapview.getOverlays().add(startMarker)
+//                        binding.mapview.controller.setCenter(startPoint)
+//                        binding.loadingText.isVisible=false
+//                        binding.geo.isVisible=true
+//                        binding.mapview.isVisible=true
+//                        binding.workId.isVisible=true
+//                        //binding.workTime.isVisible=true
+//                        binding.geo.text ="Местоположение: \n"+location.latitude.toString()+" (ш.),\n"+location.longitude.toString()+" (д.)"
+//                        val point_time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date()).toString()
+//                        workId?.let {
+//                            insertPoint(it, location.latitude, location.longitude, point_time)
+//                        }
+//
+//                    }
+//                    else if(location == null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)){
+//                        val boundingBox = BoundingBox(geoVlg.altitude*1.01, geoVlg.longitude*1.01, geoVlg.altitude*0.99, geoVlg.longitude*0.99)
+//                        binding.mapview.zoomToBoundingBox(boundingBox.increaseByScale(0.05f), false)
+//                        binding.mapview.controller.setCenter(geoVlg)
+//                        binding.geo.text ="Местоположение: не распознано"
+//                    }
+//                })
         }
     }
 
