aa432dbfeb2fab5723fed0c7457e660909567627
AnastasiaBeschastnova <baugold@mail.ru>
2024-04-25 16:56:51 +0300

    Added a ViewModel for the ContinueWorkFragment

--- app/src/main/java/com/example/agrotracker/operator/ContinueWorkFragment.kt
+++ app/src/main/java/com/example/agrotracker/operator/ContinueWorkFragment.kt
@@ -1,11 +1,7 @@
 package com.example.agrotracker.operator
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import androidx.fragment.app.Fragment
@@ -15,25 +11,18 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
-import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.agrotracker.api.NetworkService
-import com.example.agrotracker.api.requests.InsertPointRequest
-import com.example.agrotracker.api.requests.InsertWorkRequest
-import com.example.agrotracker.api.requests.UpdateWorkRequest
 import com.example.agrotracker.databinding.FragmentContinueWorkBinding
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.BoundingBox
@@ -41,26 +30,61 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.Marker
 import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ContinueWorkFragment : Fragment() {
 
-    private var workId: Int ?= null
     private var _binding: FragmentContinueWorkBinding? = null
     private val args: ContinueWorkFragmentArgs by navArgs()
-    private val api by lazy{ NetworkService.instance?.agroTrackerApi}
+    private val viewModel: ContinueWorkViewModel by viewModels()
     private val geoVlg = GeoPoint(48.7070, 44.5169)//Волгоград
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiData.collect {
+                    when (it) {
+                        is ContinueWorkViewModel.Data.UiData -> {
+                            setUpUiData(it)
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiAction.collect {
+                    when (it) {
+                        is ContinueWorkViewModel.Actions.ShowToast -> {
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        }
+
+                        is ContinueWorkViewModel.Actions.NavigateToEndWorkFormFragment -> {
+                            findNavController().navigate(
+                                ContinueWorkFragmentDirections
+                                    .actionOperatorThirdFragmentToOperatorFourthFragment(
+                                        it.workId, it.endTime, args.workTypeId.toInt()
+                                    )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentContinueWorkBinding.inflate(inflater, container, false)
@@ -71,58 +95,23 @@ class ContinueWorkFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        selectWorkId(args.creatorId,args.startTime)
+        viewModel.selectWorkId(args.creatorId, args.startTime)
         binding.buttonEnd.setOnClickListener {
-            val endTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date())
-            val values = binding.workId.text.split(": ")
-            val workId = values[1].toInt()
-            updateWork(workId,endTime)
+            viewModel.sendUpdatedWork(
+                workId = binding.workId.text.split(": ")[1].toInt(),
+                endTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date()),
+                workTypeId = args.workTypeId
+            )
         }
+
+        listenLocation()
 
         binding.mapview.setTileSource(TileSourceFactory.MAPNIK)
         binding.mapview.controller.setZoom(17.0)
-
-        listenLocation()
-        startTimer()
         Toast.makeText(requireContext(), "Карта загружается. Подождите", Toast.LENGTH_LONG).show()
 
     }
 
-
-    private fun selectWorkId(creatorId: Int, startTime: String) {
-        CoroutineScope(Dispatchers.Main).launch {
-            flow{
-                val selectWorkIdResponse = api?.selectWorkId(creatorId,startTime)
-                emit(selectWorkIdResponse)
-            }.catch { e ->
-                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
-            }.collect { selectWorkIdResponse ->
-                if(selectWorkIdResponse?.work_id!=null)
-                {
-                    workId=selectWorkIdResponse.work_id
-                    binding.workId.text = "ID работы: "+workId.toString()
-                }
-            }
-        }
-    }
-
-    private fun updateWork(workId: Int, endTime: String) {
-        CoroutineScope(Dispatchers.Main).launch {
-            flow{
-                val updateWorkResponse = api?.updateWork(
-                    UpdateWorkRequest(workId,endTime)
-                )
-                emit(updateWorkResponse)
-            }.catch { e ->
-                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
-            }.collect { updateWorkResponse ->
-                findNavController().navigate(
-                    ContinueWorkFragmentDirections
-                        .actionOperatorThirdFragmentToOperatorFourthFragment(workId,endTime,args.workTypeId)
-                )
-            }
-        }
-    }
 
     private fun listenLocation() {
         if (ActivityCompat.checkSelfPermission(
@@ -133,9 +122,11 @@ class ContinueWorkFragment : Fragment() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            val fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(requireActivity())
             fusedLocationProviderClient.requestLocationUpdates(
-                LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).setWaitForAccurateLocation(false)
+                LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+                    .setWaitForAccurateLocation(false)
                     .setMinUpdateIntervalMillis(500)
                     .setMaxUpdateDelayMillis(1000)
                     .build(),
@@ -144,73 +135,47 @@ class ContinueWorkFragment : Fragment() {
                         p0 ?: return
                         for (location in p0.locations) {
                             if (location != null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
-                                val startPoint = GeoPoint(location.latitude, location.longitude)
-                                val startMarker = Marker(binding.mapview)
-                                startMarker.setPosition(startPoint)
-                                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                                binding.mapview.getOverlays().clear()
-                                binding.mapview.getOverlays().add(startMarker)
-                                binding.mapview.controller.setCenter(startPoint)
-                                binding.loadingText.isVisible=false
-                                binding.geo.isVisible=true
-                                binding.mapview.isVisible=true
-                                binding.workId.isVisible=true
-                                binding.workTime.isVisible=true
-                                binding.geo.text ="Местоположение: \n"+location.latitude.toString()+" (ш.),\n"+location.longitude.toString()+" (д.)"
-                                val point_time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date()).toString()
-                                workId?.let {
-                                    insertPoint(it, location.latitude, location.longitude, point_time)
-                                }
-
-                            }
-                            else if(location == null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)){
-                                val boundingBox = BoundingBox(geoVlg.altitude*1.01, geoVlg.longitude*1.01, geoVlg.altitude*0.99, geoVlg.longitude*0.99)
-                                binding.mapview.zoomToBoundingBox(boundingBox.increaseByScale(0.05f), false)
-                                binding.mapview.controller.setCenter(geoVlg)
-                                binding.geo.text ="Местоположение: не распознано"
+                                viewModel.sendPoint(location)
                             }
                         }
                     }
                 },
                 Looper.getMainLooper()
-                )
+            )
         }
     }
 
-    private fun insertPoint(
-        workId: Int, lat: Double, lon: Double, pointTime: String
-    ) {
-        CoroutineScope(Dispatchers.Main).launch {
-            flow {
-                val insertPointResponse = api?.insertPoint(
-                    InsertPointRequest(
-                        workId,
-                        lat,
-                        lon,
-                        pointTime
-                    )
-                )
-                emit(insertPointResponse)
-            }.catch { e ->
-                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
-            }.collect { insertPointResponse ->
-                println(insertPointResponse)
-            }
-        }
-    }
+    private fun setUpUiData(data: ContinueWorkViewModel.Data.UiData) {
+        val location = data.location
+        binding.workTime.text = "В пути: " + data.currentTime
 
-    private fun startTimer() = CoroutineScope(Dispatchers.Main).launch{
-        while(lifecycle.currentState == Lifecycle.State.RESUMED){
+        binding.workId.text = "ID работы: " + data.workId.toString()
 
-            val nowTime=Date()
-            val startTime=SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").parse(args.startTime)
-            //разница в миллисекундах - перевод в часы, минуты, секунды, миллисекунды
-            val pathTime = kotlin.math.abs(nowTime.time - startTime.time)//миллисекунды
-            val path = SimpleDateFormat("HH:mm:ss" ).apply {
-                timeZone = TimeZone.getTimeZone("GMT+00:00")
-            }.format(Date(pathTime))
-            binding.workTime.text = "В пути: "+path
-            delay(1000)
+        if (location != null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
+            val startPoint = GeoPoint(location.latitude, location.longitude)
+            val startMarker = Marker(binding.mapview)
+            startMarker.setPosition(startPoint)
+            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            binding.mapview.getOverlays().clear()
+            binding.mapview.getOverlays().add(startMarker)
+            binding.mapview.controller.setCenter(startPoint)
+            binding.loadingText.isVisible = false
+            binding.geo.isVisible = true
+            binding.mapview.isVisible = true
+            binding.workId.isVisible = true
+            binding.workTime.isVisible = true
+            binding.geo.text =
+                "Местоположение: \n" + location.latitude.toString() + " (ш.),\n" + location.longitude.toString() + " (д.)"
+        } else if (location == null && (lifecycle.currentState == Lifecycle.State.STARTED || lifecycle.currentState == Lifecycle.State.RESUMED)) {
+            val boundingBox = BoundingBox(
+                geoVlg.altitude * 1.01,
+                geoVlg.longitude * 1.01,
+                geoVlg.altitude * 0.99,
+                geoVlg.longitude * 0.99
+            )
+            binding.mapview.zoomToBoundingBox(boundingBox.increaseByScale(0.05f), false)
+            binding.mapview.controller.setCenter(geoVlg)
+            binding.geo.text = "Местоположение: не распознано"
         }
     }
 
---
+++ app/src/main/java/com/example/agrotracker/operator/ContinueWorkViewModel.kt
@@ -0,0 +1,154 @@
+package com.example.agrotracker.operator
+
+import android.location.Location
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.agrotracker.api.NetworkService
+import com.example.agrotracker.api.requests.InsertPointRequest
+import com.example.agrotracker.api.requests.UpdateWorkRequest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TimeZone
+
+class ContinueWorkViewModel : ViewModel() {
+
+    private var workId: Int? = null
+    private var location: Location? = null
+    private val api by lazy { NetworkService.instance?.agroTrackerApi }
+    private val _uiData = MutableStateFlow<ContinueWorkViewModel.Data?>(null)
+    val uiData: StateFlow<ContinueWorkViewModel.Data?> = _uiData.asStateFlow()
+    private var timerIsStarted = false
+    private var startTime: Date = Date()
+    private var currentTime: Date = Date()
+
+    private val _uiAction = MutableSharedFlow<ContinueWorkViewModel.Actions>()
+    val uiAction: SharedFlow<ContinueWorkViewModel.Actions> = _uiAction.asSharedFlow()
+
+    fun sendUpdatedWork(workId: Int, endTime: String, workTypeId: Int) = viewModelScope.launch {
+        updateWork(workId, endTime, workTypeId)
+        stopTimer()
+    }
+
+    private fun updateWork(workId: Int, endTime: String, workTypeId: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            flow {
+                val updateWorkResponse = api?.updateWork(
+                    UpdateWorkRequest(workId, endTime)
+                )
+                emit(updateWorkResponse)
+            }.catch { e ->
+                _uiAction.emit(Actions.ShowToast(e.message.orEmpty()))
+            }.collect { updateWorkResponse ->
+                _uiAction.emit(Actions.NavigateToEndWorkFormFragment(endTime, workId, workTypeId))
+            }
+        }
+    }
+
+    fun sendPoint(location: Location) = viewModelScope.launch {
+        this@ContinueWorkViewModel.location = location
+        updateUiData()
+        insertPoint(location.latitude, location.longitude)
+    }
+
+    private fun insertPoint(
+        lat: Double, lon: Double
+    ) {
+        CoroutineScope(Dispatchers.Main).launch {
+            updateUiData()
+            workId?.let { workIdNotNull ->
+                flow {
+                    val insertPointResponse = api?.insertPoint(
+                        InsertPointRequest(
+                            workIdNotNull,
+                            lat,
+                            lon,
+                            SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").format(Date())
+                        )
+                    )
+                    emit(insertPointResponse)
+                }.catch { e ->
+                    _uiAction.emit(Actions.ShowToast(e.message.orEmpty()))
+                }.collect { insertPointResponse ->
+                    println(insertPointResponse)
+                }
+            }
+        }
+    }
+
+    fun selectWorkId(creatorId: Int, startTime: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@ContinueWorkViewModel.startTime =
+                SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX").parse(startTime)
+            flow {
+                val selectWorkIdResponse = api?.selectWorkId(creatorId, startTime)
+                emit(selectWorkIdResponse)
+            }.catch { e ->
+                _uiAction.emit(Actions.ShowToast(e.message.orEmpty()))
+            }.collect { selectWorkIdResponse ->
+                if (selectWorkIdResponse?.work_id != null) {
+                    workId = selectWorkIdResponse.work_id
+                    startTimer()
+                }
+            }
+        }
+    }
+
+    private fun updateUiData() = viewModelScope.launch {
+        workId?.let {
+            if (location != null)
+                _uiData.value = Data.UiData(
+                    workId = it,
+                    currentTime = SimpleDateFormat("HH:mm:ss").apply {
+                        timeZone = TimeZone.getTimeZone("GMT+00:00")
+                    }.format(currentTime),
+                    location = location
+                )
+        }
+    }
+
+    private fun startTimer() = CoroutineScope(Dispatchers.Main).launch {
+        timerIsStarted = true
+        while (timerIsStarted) {
+            val nowTime = Date()
+            val pathTime = kotlin.math.abs(nowTime.time - startTime.time)//миллисекунды
+            currentTime = Date(pathTime)
+            updateUiData()
+            delay(1000)
+        }
+    }
+
+    private fun stopTimer() {
+        timerIsStarted = false
+    }
+
+    sealed class Data {
+        class UiData(
+            val workId: Int,
+            val currentTime: String,
+            val location: Location?
+        ) : Data()
+    }
+
+    sealed class Actions {
+        class ShowToast(val message: String) : Actions()
+
+        class NavigateToEndWorkFormFragment(
+            val endTime: String,
+            val workId: Int,
+            val workTypeId: Int,
+        ) : Actions()
+    }
+}
